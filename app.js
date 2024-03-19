0ed389731d3f1b18eec8225d3989ad11b698ea2e
hammad4291 <www.hammad622h@gmail.com>
2024-03-19 15:47:59 +0500

    made new inventory div

--- app.js
+++ app.js
@@ -1,68 +1,61 @@
 let body = document.querySelector("body");
-        let inventory = document.querySelector("#inventory");
-        let goBack = document.querySelectorAll(".go-back");
-        const URL = `https://fakestoreapi.com/products`;
-        let inventoryList = document.querySelector(".inventory-list");
+const URL = `https://fakestoreapi.com/products`;
+let inventoryBox = document.querySelector("#int-box");
 
-        window.addEventListener("load", () => {
-            products();
-        })
+window.addEventListener("load", () => {
+    products();
+});
 
-        const products = async () => {
-            inventory.style.display = "none";
-            let response = await fetch(URL);
-            let data = await response.json();
-            console.log(data);
-            data.forEach((ele) => {
-                createElement(ele);
-            });
+const products = async () => {
+    try {
+        let response = await fetch(URL);
+        let data = await response.json();
+        console.log(data);
+        data.forEach((element) => {
+            createElement(element);
+        });
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+    
+};
 
-            goBack.forEach((button) => {
-                button.addEventListener("click", () => {
-                    inventoryList.style.transform = "translateX(100%)"; // Move inventory-list back to its initial position
-                    inventory.style.display = "none";
-                });
-            });
-        }
-
-        function createElement(element) {
-            console.log(element);
-            console.log(element.image);
-            let imgsrc = element.image;
-            let cat = element.category;
-            let Title = element.title;
-            let Price = element.price;
-            let Rating = element.rating.rate;
-            let box = document.createElement("div");
-            body.append(box);
-            box.className = 'box';
-            let img = document.createElement("img");
-            box.append(img);
-            img.src = imgsrc;
-            img.backgroundSize = "cover";
-            let category = document.createElement("p");
-            category.className = "category";
-            category.innerText = cat;
-            box.append(category);
-            let title = document.createElement("p");
-            title.className = "Title";
-            title.innerText = Title;
-            box.append(title);
-            let price = document.createElement("p");
-            price.className = "price";
-            price.innerText = Price;
-            box.append(price);
-            let rating = document.createElement("p");
-            rating.className = "rating";
-            rating.innerText = Rating;
-            box.append(rating);
-            let cartButton = document.createElement("button");
-            cartButton.className = "add-cart";
-            cartButton.innerText = "Add To Cart";
-            box.append(cartButton);
-            // cartButton.addEventListener("click", () => {
-            //     console.log(inventory);
-            //     // inventoryList.style.transform = "translateX(-200%)"; // Move inventory-list onto the screen
-            //     inventory.style.display = "block";
-            // });
-        }
+function createElement(element) {
+    console.log(element);
+    let imgsrc = element.image;
+    let cat = element.category;
+    let Title = element.title;
+    let Price = element.price;
+    let Rating = element.rating.rate;
+    let box = document.createElement("div");
+    box.className = 'box';
+    let img = document.createElement("img");
+    img.src = imgsrc;
+    img.style.backgroundSize = "cover";
+    box.appendChild(img);
+    let category = document.createElement("p");
+    category.className = "category";
+    category.innerText = cat;
+    box.appendChild(category);
+    let title = document.createElement("p");
+    title.className = "Title";
+    title.innerText = Title;
+    box.appendChild(title);
+    let price = document.createElement("p");
+    price.className = "price";
+    price.innerText = Price;
+    box.appendChild(price);
+    let rating = document.createElement("p");
+    rating.className = "rating";
+    rating.innerText = Rating;
+    box.appendChild(rating);
+    let cartButton = document.createElement("button");
+    cartButton.className = "add-cart";
+    cartButton.innerText = "Add To Cart";
+    box.appendChild(cartButton);
+    body.appendChild(box);
+    inventoryBox.style.display="none";
+    cartButton.addEventListener( "click" , () => {
+        inventoryBox.style.display="block";
+    })
+}
